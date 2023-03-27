318b359880023142b002f3a4f006e879eb62da18
Darian <darianychen@gmail.com>
2023-03-26 22:27:07 -0400

    Add seed for sun

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do 
+    puts "Destroying tables..."
+    # Unnecessary if using `rails db:seed:replant`
+    Star.destroy_all
+    Planet.destroy_all
+    Moon.destroy_all
+  
+    puts "Resetting primary keys..."
+    # For easy testing, so that after seeding, the first `User` has `id` of 1
+    ApplicationRecord.connection.reset_pk_sequence!('stars')
+    ApplicationRecord.connection.reset_pk_sequence!('planets')
+    ApplicationRecord.connection.reset_pk_sequence!('moons')
+  
+    puts "Creating stars..."
+    # Create one user with an easy to remember email and password:
+    Star.create!(
+        name: "Sun",
+        mass: 1.989e30,
+        facts: ["The Sun is over 4.5 billion years old.", "Many ancient civilisations thought The Sun was a God.", "Sometimes spots on The Sun erupt and mess with our mobile phone signals."],
+        diameter: 865370,
+        temperature: 5778,
+    )
+
+    puts "Done!"
+  end
