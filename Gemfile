239791e19cda377329c8ca0fd7054273f45985fe
Darian <darianychen@gmail.com>
2023-04-01 20:02:19 -0400

    Enable CORS

--- Gemfile
+++ Gemfile
@@ -33,5 +33,6 @@ group :development do
   gem "better_errors"
   gem "binding_of_caller"
   gem "pry-rails"
+  gem "rack-cors"
 end
 
--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,14 @@ module CssSolarSystemBackendMe
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add CORS headers to allow cross-origin requests from other domains
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
   end
 end
+
